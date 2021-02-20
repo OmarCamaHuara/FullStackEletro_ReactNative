bf9464fafcc1e3bcd7acf82cb14cb68bd4bc1af6
Omar Cama Huarahuara <66935801+OmarCamaHuara@users.noreply.github.com>
2021-02-19 23:08:36 -0300

    Initial commit

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  View,
+  StatusBar, 
+} from 'react-native';
+
+
+// import Routes from './src/screens/Inicio'
+// import Routes from './src/screens/Login'
+// import Routes from './src/screens/Produtos';
+
+import Routes from './src/routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <StatusBar />
+      <Routes />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
